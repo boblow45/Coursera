a6a3f743e7e40c33351f8d51603ac0d635f2a359
Cillian O'Brien <cillian.obrien@analog.com>
2018-04-10 23:52:47 +0100

    working assignment

--- cpp_as_a_netter_c/week_1/c_to_cpp.cpp
+++ cpp_as_a_netter_c/week_1/c_to_cpp.cpp
@@ -12,40 +12,21 @@
 
 #include <iostream>
 #include <vector>
-// #include <accumulate>
+#include <numeric>
 
 using namespace std; 
 
 const int vector_size  = 40;
 
-
-inline void sum(int& p, const vector<int> &d)
-{
-	p = 0;
-	
-	// interate through vector, 
-	// for(auto &n : d)
-	// 	p += n;
-	for (int i=0; i < d.size(); i++)
-       p += d[i];
-
-}
-
-
-int main()
-{
+int main(){
 	vector<int> data(vector_size);
 
-	for(int i = 0; i < data.size(); ++i)
+	for(int i = 0; i < data.size(); ++i) 
 		data.at(i) = i;
-
-	int accum = 0;
-	sum(accum, data);
-
-//	int accum = 0;
-//	accum = accumulate(data.begin(), data.end(), 0);
 	
-	cout << "sum is" << accum << endl; 
+	int accum = accumulate(data.begin(), data.end(), 0);
+
+	cout << "Sum is " << accum << endl; 
 
 	return 0;
 }
